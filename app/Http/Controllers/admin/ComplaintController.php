264fec138bcf7f89d0fda01f56eb3714f957600d
fan724 <fanil724@gmail.com>
2025-02-20 21:16:37 +0300

    add function

--- app/Http/Controllers/admin/ComplaintController.php
+++ app/Http/Controllers/admin/ComplaintController.php
@@ -26,4 +26,28 @@ class ComplaintController extends Controller
         $comInstrukt = ComplaintsInstructions::paginate(10);
         return view('admin.complaint.index', ['comInstrukts' => $comInstrukt]);
     }
+
+
+    public function status(string $id)
+    {
+        $сomplaints = ComplaintsInstructions::find($id);
+        if ($сomplaints) {
+            $сomplaints->status = (($сomplaints->status == 1) ? 0 : 1);
+            if ($сomplaints->save()) {
+                return response()->json([
+                    'success' => 'true',
+                    'message' => 'Статус жалобы успешно иземенен',
+                ]);
+            }
+            return response()->json([
+                'success' => false,
+                'message' => 'Статус жалобы не иземенен'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Жалоба не найдена'
+        ], 404);
+    }
 }
